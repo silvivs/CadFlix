05564b5087209cc431fab9f64b4f25a37d7a8bff
Saoterus <jophslv@gmail.com>
2023-05-28 00:25:54 -0400

    atualizações do cabeçalho e do arquivo Funcoes.c

--- Funcoes.c
+++ Funcoes.c
@@ -1,14 +1,15 @@
 #include "Cabecalho.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct filme {
   char nome[81];
-  float preco = 0.0;
+  float preco;
   char diretor[81];
-  int ano = 0;
+  int ano;
   char genero[81];
-  int idade = 0;
+  int idade;
 };
 
 Filme* criar_filme() {
@@ -27,11 +28,35 @@ void free_filme(Filme* f) {
 
 void imprimir_filme(Filme* f) {
 
-  printf("Nome: %s\n", f.nome);
-  printf("Preço: %.2f\n", f.preco):
-  printf("Diretor: %s\n", f.diretor);
-  printf("Ano: %d\n", f.ano);
-  printf("Gênero: %s\n", f.genero);
-  printf("Faixa etária: %d\n", f.idade);
+  printf("Nome: %s\n", f->nome);
+  printf("Preço: %.2f\n", f->preco);
+  printf("Diretor: %s\n", f->diretor);
+  printf("Ano: %d\n", f->ano);
+  printf("Gênero: %s\n", f->genero);
+  printf("Faixa etária: %d\n", f->idade);
 
+}
+
+void set_nome(Filme* f, char* novo){
+  strcpy(f->nome, novo);
+}
+
+void set_preco(Filme* f, float novo){
+  f->preco = novo;
+}
+
+void set_diretor(Filme* f, char* novo){
+  strcpy(f->diretor, novo);
+}
+
+void set_genero(Filme* f, char* novo){
+  strcpy(f->genero, novo);
+}
+
+void set_ano(Filme* f, int novo){
+  f->ano = novo;
+}
+
+void set_faixaetaria(Filme* f, int novo){
+  f->idade = novo;
 }
