06c2d458600d3fd059deca3ffcdb1eaa6f1099c2
silvivs <jophslv@gmail.com>
2023-05-29 15:33:25 -0400

    atualizações gerais, submenu pronto

--- Main.c
+++ Main.c
@@ -22,111 +22,133 @@ int main(void) {
     getchar();
 
     switch (op) {
-    case 1:
-      tabela[index] = criar_filme();
-      // leitura(tabela, index);
-
-      printf("Nome: ");
-      char nome[101];
-      fgets(nome, sizeof(nome), stdin);
-      nome[strcspn(nome, "\n")] = '\0';
-      set_nome(tabela[index], nome);
-
-      printf("Preco: ");
-      float preco = 0;
-      scanf("%f", &preco);
-      getchar();
-      set_preco(tabela[index], preco);
-
-      printf("Diretor: ");
-      char diretor[101];
-      fgets(diretor, sizeof(diretor), stdin);
-      diretor[strcspn(diretor, "\n")] = '\0';
-      set_diretor(tabela[index], diretor);
-
-      printf("Ano: ");
-      int ano = 0;
-      scanf("%d", &ano);
-      getchar();
-      set_ano(tabela[index], ano);
-
-      printf("Genero: ");
-      char genero[51];
-      fgets(genero, sizeof(genero), stdin);
-      genero[strcspn(genero, "\n")] = '\0';
-      set_genero(tabela[index], genero);
-
-      int faixaEtaria = 0;
-
-      if (procuraSubstring(genero) == 1) {
-        printf("Faixa etaria: ");
-        while (scanf("%d", &faixaEtaria), faixaEtaria < 18) {
-          printf("Filmes de Terror nao podem ter classificacao para menores de 18 anos!Digite outro valor: ");
-        }
-
-        set_faixaetaria(tabela[index], faixaEtaria);
-      }
-      else {
-        printf("Faixa etaria: ");
-        scanf("%d", &faixaEtaria);
-        set_faixaetaria(tabela[index], faixaEtaria);
-      }
-
-      print_tabela(tabela, index + 1);
-      index++;
-      break;
-
-    case 2:
-
-      subMenu();
-
-      int sub_op2 = 0;
-      scanf("%d", &sub_op2);
-
-      switch (sub_op2) {
       case 1:
-        int id = 0;
-        printf("Informe o codigo do filme que sera renomeado: ");
-        scanf("%d", &id);
-        getchar();
+        tabela[index] = criar_filme();
+        // leitura(tabela, index);
 
         printf("Nome: ");
         char nome[101];
         fgets(nome, sizeof(nome), stdin);
         nome[strcspn(nome, "\n")] = '\0';
-        set_nome(tabela[id - 1], nome);
+        set_nome(tabela[index], nome);
+
+        printf("Preco: ");
+        float preco = 0;
+        scanf("%f", &preco);
+        getchar();
+        set_preco(tabela[index], preco);
 
         printf("Diretor: ");
         char diretor[101];
         fgets(diretor, sizeof(diretor), stdin);
         diretor[strcspn(diretor, "\n")] = '\0';
-        set_diretor(tabela[id - 1], diretor);
+        set_diretor(tabela[index], diretor);
 
-        print_tabela(tabela, index);
+        printf("Ano: ");
+        int ano = 0;
+        scanf("%d", &ano);
+        getchar();
+        set_ano(tabela[index], ano);
 
+        printf("Genero: ");
+        char genero[51];
+        fgets(genero, sizeof(genero), stdin);
+        genero[strcspn(genero, "\n")] = '\0';
+        set_genero(tabela[index], genero);
+
+        int faixaEtaria = 0;
+
+        if (procuraSubstring(genero) == 1) {
+          printf("Faixa etaria: ");
+          while (scanf("%d", &faixaEtaria), faixaEtaria < 18) {
+            printf("Filmes de Terror nao podem ter classificacao para menores de 18 anos!Digite outro valor: ");
+          }
+
+          set_faixaetaria(tabela[index], faixaEtaria);
+        }
+        else {
+          printf("Faixa etaria: ");
+          scanf("%d", &faixaEtaria);
+          set_faixaetaria(tabela[index], faixaEtaria);
+        }
+
+        print_tabela(tabela, index + 1);
+        index++;
         break;
 
       case 2:
+
+        subMenu();
+        int id = 0;
+        int sub_op2 = 0;
+        scanf("%d", &sub_op2);
+
+        switch (sub_op2) {
+          case 1:
+            printf("Informe o codigo do filme que sera renomeado: ");
+            scanf("%d", &id);
+            getchar();
+
+            printf("Nome: ");
+            char nome[101];
+            fgets(nome, sizeof(nome), stdin);
+            nome[strcspn(nome, "\n")] = '\0';
+            set_nome(tabela[id - 1], nome);
+
+            printf("Diretor: ");
+            char diretor[101];
+            fgets(diretor, sizeof(diretor), stdin);
+            diretor[strcspn(diretor, "\n")] = '\0';
+            set_diretor(tabela[id - 1], diretor);
+
+            print_tabela(tabela, index);
+
+            break;
+
+          case 2:
+            printf("Informe o codigo do filme que tera o preco atualizado: ");
+            scanf("%d", &id);
+            getchar();
+
+            printf("Preco: ");
+            float preco = 0;
+            scanf("%f", &preco);
+            getchar();
+            set_preco(tabela[id - 1], preco);
+
+            print_tabela(tabela, index);
+
+            break;
+
+          case 3:
+            printf("Informe o codigo do filme que tera o genero atualizado: ");
+            scanf("%d", &id);
+            getchar();
+
+            printf("Genero: ");
+            char genero[51];
+            fgets(genero, sizeof(genero), stdin);
+            genero[strcspn(genero, "\n")] = '\0';
+            set_genero(tabela[id - 1], genero);
+
+            print_tabela(tabela, index);        
+
+            break;
+
+          default:
+            printf("Opcao invalida\n");
+            break;
+        }
+
         break;
 
       case 3:
         break;
 
       default:
-        printf("Opcao invalida\n");
+        printf("Opcao invalida!\n\n");
         break;
-      }
-
-      break;
-
-    case 3:
-      break;
-
-    default:
-      printf("Opcao invalida!\n\n");
-      break;
     }
-
     menu();
   }
 
